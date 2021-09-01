f0adc5ef1d167c015765f2fe2f51f52f05e606d6
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-09-01 06:32:56 -0400

    Got all the code but there is a COORS error preventing the api from being used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import * as tt from '@tomtom-international/web-sdk-maps';
+import * as ttapi from '@tomtom-international/web-sdk-services';
 import './App.css';
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
@@ -41,7 +42,50 @@ const App = () => {
   });
  };
 
+ // POINT CONVERTER
+ const convertToPoints = (lngLat) => {
+  return {
+   point: {
+    latitude: lngLat.lat,
+    longitude: lngLat.lng
+   }
+  };
+ };
+
+ const addDeliveryMarker = (lngLat, map) => {
+  const element = document.createElement('div');
+  element.className = 'marker-delivery';
+  new tt.Marker({
+   element: element
+  })
+   .setLngLat(lngLat)
+   .addTo(map);
+ };
+
+ const drawRoute = (geoJSON, map) => {
+  if (map.getLayer('route')) {
+   map.removeLayer('route');
+   map.removeSource('route');
+  }
+  map.addLayer({
+   id: 'route',
+   type: 'line',
+   source: {
+    type: 'geojson',
+    data: geoJSON
+   },
+   paint: {
+    'line-color': 'red',
+    'line-width': 6
+   }
+  });
+ };
+
  useEffect(() => {
+  const origin = {
+   lng: longitude,
+   lat: latitude
+  };
 
   geoLocator();
 
@@ -84,8 +128,56 @@ const App = () => {
 
   addMarker();
 
-  return new Promise((resolve, reject) => {
+  const sortDestinations = (locations) => {
+   const pointsForDestinations = locations.map((destination) => {
+    return convertToPoints(destination);
+   });
+   const callParameters = {
+    api: process.env.REACT_APP_TOM_TOM_API_KEY,
+    destinations: pointsForDestinations,
+    origins: [convertToPoints(origin)]
+   };
+   return new Promise((resolve, reject) => {
+    ttapi.services.matrixRouting(callParameters)
+     .then((matrixAPIResults) => {
+      const results = matrixAPIResults.matrix[0];
+      const resultsArray = results.map((result, index) => {
+       return {
+        location: locations[index],
+        drivingTime: result.response.routeSummary.travelTimeInSeconds
+       };
+      });
+      resultsArray.sort((a, b) => {
+       return a.drivingTime - b.drivingTime;
+      });
+      const sortedLocations = resultsArray.map((result) => {
+       return result.location;
+      });
+      resolve(sortedLocations);
+     });
+   });
+  };
 
+  const recalculateRoutes = () => {
+   sortDestinations(destinations).then((sorted) => {
+    sorted.unshift(origin);
+    ttapi.services
+     .calculateRoute({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      locations: sorted
+     }).then((routData) => {
+      const geoJSON = routData.toGeoJson();
+      drawRoute(geoJSON, map);
+     });
+   });
+  };
+
+  // CREATE A DESTINATIONS ARRAY AND PUSH ANYWHERE THAT IS CLICKED INTO THE ARRAY
+  const destinations = [];
+  map.on('click', (e) => {
+   destinations.push(e.lngLat);
+   addDeliveryMarker(e.lngLat, map);
+   recalculateRoutes();
   });
 
   return () => map.remove();
